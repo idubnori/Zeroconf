99f2f250617098eb08184c7aaf9ca89a8ddc3ab2
idubnori <i.dub.nori@gmail.com>
2018-02-12 00:06:39 +0900

    Added test console app.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Zeroconf;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(async () =>
+            {
+                var domains = await ZeroconfResolver.BrowseDomainsAsync();
+                foreach (var domain in domains)
+                {
+                    Console.WriteLine($"key -> {domain.Key}");
+                }
+
+                var responses = await ZeroconfResolver.ResolveAsync(domains.Select(g => g.Key));
+                foreach (var resp in responses)
+                    Console.WriteLine(resp);
+            }).Wait();
+        }
+    }
+}
